6150c9caa622d0cd15bb5dd13fdcc0b8ea1f0093
flyboy <852818393@qq.com>
2019-04-13 20:35:57 +0800

    123123

--- js/work.js
+++ js/work.js
@@ -1,11 +1,11 @@
 $('#send_verify_button').click(function(){
     var url = $('.form-widget').find('form').attr();
-    var mobile = $('.form-widget').find('.mobile').val();
-    console.log(mobile);
+    //var mobile = $('.form-widget').find('.mobile').val();
+   // console.log(mobile);
     var type = 'moblie';
     var data ={'mobile':mobile,'type':type};
 
-    $.post(url,{mobile:mobile,type:'moblie'},function(result){
+    $.post(url,{mobile:'123123123',type:'moblie'},function(result){
         console.log(result);
         if(result.code == 1){
             alert(result.msg)
