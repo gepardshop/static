a2d9c8dc6c803e3d4a5c48c20b3b743d3f7223f2
flyboy <852818393@qq.com>
2019-04-13 20:44:31 +0800

    123

--- js/work.js
+++ js/work.js
@@ -1,10 +1,11 @@
 $('#send_verify_button').click(function(){
     var url = $('.form-widget').find('form').attr('notify_api');
     var mobile = $('.form-widget').find('input[name*="mobile"]').val();
-     console.log(mobile);
 
+     var data = {mobile:mobile,type:'mobile'}
+        console.log(data);
 
-    $.post(url,{mobile:'mobile',type:'mobile'},function(result){
+    $.post(url,data,function(result){
         console.log(result);
         if(result.code == 1){
             alert(result.msg)
