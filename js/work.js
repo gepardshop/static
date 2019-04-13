b0248462d9566379a0df36072fe8d9426cf174cf
flyboy <852818393@qq.com>
2019-04-13 20:29:24 +0800

    123213

--- js/work.js
+++ js/work.js
@@ -1,4 +1,4 @@
-$('#send_verify_button').onclick(function(){
+$('#send_verify_button').click(function(){
     var url = $('.form-widget').find('form').attr('');
     var mobile = $('.form-widget').find('.mobile').val();
     var type = 'moblie';
