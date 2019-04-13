2c77a64d80ccea75bc6dff8e3dad390febb3eada
flyboy <852818393@qq.com>
2019-04-13 20:30:58 +0800

    123

--- js/work.js
+++ js/work.js
@@ -5,6 +5,7 @@ $('#send_verify_button').click(function(){
     var data ={'mobile':mobile,'type':type};
 
     $.post(url,data,function(result){
+        console.log(result);
         if(result.status == 1){
             alert(result.msg)
         }else{
