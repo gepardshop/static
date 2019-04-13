215ed15ffb46359388dce05b9355987afdd02696
flyboy <852818393@qq.com>
2019-04-13 20:26:58 +0800

    js

---
+++ js/work.js
@@ -0,0 +1,17 @@
+$('#send_verify_button').onclick(funciton(){
+    var url = $('.form-widget').find('form').attr('');
+    var mobile = $('.form-widget').find('.mobile').val();
+    var type = 'moblie';
+    var data ={'mobile':mobile,'type':type};
+
+    $.post(url,data,function(result){
+        if(result.status == 1){
+            alert(result.msg)
+        }else{
+            alert(result.msg)
+        }
+
+    });
+
+
+});
